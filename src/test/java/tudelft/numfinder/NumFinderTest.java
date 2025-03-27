eb11c01185aed4c4c2c34459ddbaa1256e17b77c
Ariana <yayanhs16@gmail.com>
2025-03-27 10:42:01 -0500

    test: agregar prueba con números en orden descendente

--- src/test/java/tudelft/numfinder/NumFinderTest.java
+++ src/test/java/tudelft/numfinder/NumFinderTest.java
@@ -7,11 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class NumFinderTest {
 
     @Test
-    void testFindWithMixedNumbers() {
+    void testFindWithDescendingNumbers() {
         NumFinder nf = new NumFinder();
-        nf.find(new int[]{4, 25, 7, 9});
-        assertEquals(25, nf.getLargest());
-        assertEquals(4, nf.getSmallest());
+        nf.find(new int[]{4, 3, 2, 1});
+        assertEquals(4, nf.getLargest());
+        assertEquals(1, nf.getSmallest());
     }
-}
 
